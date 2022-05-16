cbf5c91ee56a2f75cce750b29e5f4a8909675c1e
Ergün Karakuş <ergun_karakus@hotmail.com>
2022-05-16 14:10:34 +0300

    ilk yukleme

---
+++ Models/AppUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+
+    public class UygulamaKullanicisi : IdentityUser
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+    }
